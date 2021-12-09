c0d60b0c627937b9e45f6b44ddd137073d86c8ff
Amit Dharmale <amit.20july@gmail.com>
2021-12-09 14:43:28 +0530

    added from GitPractice2

--- src/main/java/framework/App.java
+++ src/main/java/framework/App.java
@@ -4,11 +4,17 @@ package framework;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	//just some changes
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		// just some changes
+		System.out.println("Hello World!");
+
+		//Added from GitPractice2
+		System.out.println("Hello World!");
+
+		System.out.println("Hello World!");
+
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
 }
